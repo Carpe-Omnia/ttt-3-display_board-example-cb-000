8c9a906b8985309fa15aaad718d0aac26f2e3aa7
Tomaz Rodrigues <tomaz.r.rodrigues@gmail.com>
2018-11-15 23:01:16 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,10 +2,6 @@
 empty_cell = "   "
 line = "-----------"
 def display_board
-  puts " #{empty_cell} | #{empty_cell} | #{empty_cell} "
-  puts line
-  puts "#{empty_cell}|#{empty_cell}|#{empty_cell}"
-  puts line
-  puts "#{empty_cell}|#{empty_cell}|#{empty_cell}"
+  puts " #{empty_cell} "
 end
 display_board
